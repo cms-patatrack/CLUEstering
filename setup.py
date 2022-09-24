fe253bb9bb6de52f0e58d610ee43902fdf253800
SimonB00 <simone.balducci00@gmail.com>
2022-09-24 22:44:15 +0200

    Added dependencies

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
-import sys
-
-from pybind11 import get_cmake_dir
-# Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension, build_ext
 from setuptools import setup
 
-__version__ = "1.0.0"
-
-# The main interface is through Pybind11Extension.
-# * You can add cxx_std=11/14/17, and then build_ext can be removed.
-# * You can set include_pybind11=false to add the include directory yourself,
-#   say from a submodule.
-#
-# Note:
-#   Sort input source files if you glob sources to ensure bit-for-bit
-#   reproducible builds (https://github.com/pybind/python_example/pull/53)
+__version__ = "1.1.1"
 
 setup(
     name="CLUEstering",
     version=__version__,
     author="Simone Balducci",
     author_email="simone.balducci00@gmail.com",
-    description="",
-    long_description="",
+    description="A library that generalizes the original 2-dimensional CLUE algorithm made at CERN.",
+	 install_requires=['sklearn','numpy','matplotlib','pandas']
 )
