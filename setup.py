5ebcb7fb0e430f632180e25257b2439cab544ea7
SimonB00 <simone.balducci00@gmail.com>
2022-09-25 11:27:40 +0200

    Trying a single library

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
+import codecs
+import os
 
 __version__ = "1.1.1"
+
+ext_modules = [
+	Pybind11Extension("CLUEsteringCPP",
+		["CLUEstering/binding.cc"]
+	),
+]
 
 setup(
     name="CLUEstering",
@@ -8,5 +17,14 @@ setup(
     author="Simone Balducci",
     author_email="simone.balducci00@gmail.com",
     description="A library that generalizes the original 2-dimensional CLUE algorithm made at CERN.",
-	 install_requires=['sklearn','numpy','matplotlib','pandas']
+	 packages=find_packages(),
+	 install_requires=['sklearn','numpy','matplotlib','pandas'],
+	 keywords=['Python','Clustering','Binding'],
+	 classifiers=[
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 3',
+		'Operating System :: Unix',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+	 ]
 )
