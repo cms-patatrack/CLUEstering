4995fbb64f61bd324ad9635e2e08ebfb01992fb8
SimonB00 <simone.balducci@gmail.com>
2022-09-26 17:13:14 +0200

    Updated python dependency for time_ns

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 from pybind11.setup_helpers import Pybind11Extension
+from pathlib import Path
 import codecs
 import os
 
 __version__ = "1.1.17"
-long_description = ''
+this_directory = Path(__file__).parent
+long_description = (this_directory/'README.md').read_text() 
 
 ext_modules = [
 	Pybind11Extension(
@@ -20,10 +22,12 @@ setup(
     author_email="simone.balducci00@gmail.com",
     description="A library that generalizes the original 2-dimensional CLUE algorithm made at CERN.",
 	 long_description=long_description,
+	 long_description_content_type='text/markdown',
 	 packages=find_packages(),
-	 install_requires=['sklearn','numpy','matplotlib','pandas','pybind11'],
+	 install_requires=['sklearn','numpy','matplotlib','pandas'],
 	 ext_modules=ext_modules,
 	 keywords=['Python','Clustering','Binding'],
+	 python_requires='>=3.7',
 	 classifiers=[
 		'Intended Audience :: Developers',
 		'Programming Language :: Python :: 3',
