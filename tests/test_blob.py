897410f644cecb5368424ddd47be4c9bd024773b
SimonB00 <simone.balducci@gmail.com>
2022-11-14 07:33:51 +0100

    Fix in blob test

--- tests/test_blob.py
+++ tests/test_blob.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pytest
+import CLUEstering as clue
 
 def test_always_passes():
     assert True
@@ -8,7 +9,6 @@ def test_always_passes():
 def blobs():
     return pd.read_csv("./blob.csv")
 
-import CLUEstering as clue
 def test_correct_number_of_blobs(blobs):
     c = clue.clusterer(1,5,1.6)
     c.readData(blobs)
