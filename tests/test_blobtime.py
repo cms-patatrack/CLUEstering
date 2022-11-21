57364c31b9227a2b77b6e8bcbf9fb484dc5d2573
SimonB00 <simone.balducci@gmail.com>
2022-11-21 14:30:51 +0100

    Added time test for blobs

--- tests/test_blobtime.py
+++ tests/test_blobtime.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import pytest
+import time
 import CLUEstering as clue
 
 @pytest.fixture
 def blobs():
     return pd.read_csv("./blob.csv")
 
-def test_correct_number_of_blobs(blobs):
+def test_time(blobs):
     c = clue.clusterer(1,5,1.6)
     c.readData(blobs)
+    start = time.time_ns()
     c.runCLUE()
-    assert c.NClusters == 4
+    finish = time.time_ns()
+
+    elapsed_time = (finish - start)/(10**9)
+    assert elapsed_time < 1.0641621732029838
