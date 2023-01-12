a7b8b4774556f7a7538f073feb9f1f1319e2f943
SimonB00 <simone.balducci@gmail.com>
2023-01-12 12:14:21 +0100

    Debugging new testing method

--- run_test.sh
+++ run_test.sh
@@ -5,11 +5,15 @@ pip install CLUEstering
 python3 -m pytest
 mv ./file.csv ./file1.csv
 
+echo "Finished first phase"
+
 # Uninstall CLUE and install the local version of CLUEstering and test it as well
 python3 -m pip uninstall CLUEstering
 pip install .
 python3 -m pytest
 mv ./file.csv ./file2.csv
+
+echo "Finished second phase"
 
 # Now we compare the two output files, and they should be identical
 if [[ $(diff file1.csv file2.csv) -eq '' ]]
