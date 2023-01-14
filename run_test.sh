4434735817eb9af6b777fe6cc2f3174550e900ce
SimonB00 <simone.balducci@gmail.com>
2023-01-15 00:10:42 +0100

    Implementation of the test completed

--- run_test.sh
+++ run_test.sh
@@ -5,17 +5,11 @@ pip install CLUEstering
 python3 -m pytest
 mv ./file.csv ./file1.csv
 
-echo "Finished first phase"
-
 # Uninstall CLUE and install the local version of CLUEstering and test it as well
-# python3 -m pip uninstall CLUEstering || (echo "assert(False)" >> test_passed.py ; python3 -m pytest)
-# python3 -m pip uninstall CLUEstering && pip install .
 yes 2>/dev/null | pip uninstall CLUEstering
 pip install .
 python3 -m pytest
 mv ./file.csv ./file2.csv
-
-echo "Finished second phase"
 
 # Now we compare the two output files, and they should be identical
 if [[ $(diff file1.csv file2.csv) -eq '' ]]
