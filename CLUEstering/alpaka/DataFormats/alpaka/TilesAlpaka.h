353013c256de3fe8ee03adaf5dafa37b62118edc
sbaldu <simone.balducci00@gmail.com>
2023-06-30 13:35:09 +0200

    Fix non trivial copyability in the TilesAlpaka class

--- CLUEstering/alpaka/DataFormats/alpaka/TilesAlpaka.h
+++ CLUEstering/alpaka/DataFormats/alpaka/TilesAlpaka.h
@@ -11,33 +11,31 @@
 #include <algorithm>
 #include <numeric>
 #include <stdint.h>
+#include <type_traits>
 
 #include "../../AlpakaCore/alpakaConfig.h"
 #include "../../AlpakaCore/alpakaMemory.h"
 #include "AlpakaVecArray.h"
-#include "../Math/Algorithms.h"
 
 using cms::alpakatools::VecArray;
 
-constexpr uint32_t max_tile_depth = 100;
+constexpr uint32_t max_tile_depth{100};
+constexpr uint32_t max_n_tiles{10000};
 
 namespace ALPAKA_ACCELERATOR_NAMESPACE {
 
-  template <typename TAcc, uint8_t Ndim>
+  template <uint8_t Ndim>
   class TilesAlpaka {
   public:
-	// Constructors
-    TilesAlpaka() = delete;
-    TilesAlpaka(const TAcc& acc) { m_acc = acc; }
 
 	// Copy constructor/assignment operator
-	TilesAlpaka(const TilesAlpaka&) = delete;
-	TilesAlpaka& operator=(const TilesAlpaka&) = delete;
-	// Move constructor/assignment operator
-	TilesAlpaka(TilesAlpaka&&) = delete;
-	TilesAlpaka& operator=(TilesAlpaka&&) = delete;
-	// Destructor
-	~TilesAlpaka() = default;
+	/* TilesAlpaka(const TilesAlpaka&) = delete; */
+	/* TilesAlpaka& operator=(const TilesAlpaka&) = delete; */
+	/* // Move constructor/assignment operator */
+	/* TilesAlpaka(TilesAlpaka&&) = default; */
+	/* TilesAlpaka& operator=(TilesAlpaka&&) = default; */
+	/* // Destructor */
+	/* ~TilesAlpaka() = default; */
 
     /* void resizeTiles() { tiles_.resize(nTiles); } */
 
@@ -45,6 +43,7 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     VecArray<float, Ndim> tile_size;
     VecArray<VecArray<float, 2>, Ndim> min_max;
 
+	template <typename TAcc>
     ALPAKA_FN_HOST_ACC inline constexpr int getBin(const TAcc& acc, float coord_, int dim_) const {
       int coord_Bin{(int)((coord_ - min_max[dim_][0]) / tile_size[dim_])};
       // Address the cases of underflow and overflow and underflow
@@ -54,17 +53,19 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       return coord_Bin;
     }
 
+	template <typename TAcc>
     ALPAKA_FN_HOST_ACC inline constexpr int getGlobalBin(const TAcc& acc, const VecArray<float, Ndim>& coords) const {
-      int globalBin{getBin(coords[0], 0)};
+      int globalBin{getBin(acc, coords[0], 0)};
       int ntiles_per_dim{(int)(alpaka::math::pow(acc, n_tiles, 1.0 / Ndim))};
       for (int i{1}; i != Ndim; ++i) {
-        globalBin += ntiles_per_dim * getBin(coords[i], i);
+        globalBin += ntiles_per_dim * getBin(acc, coords[i], i);
       }
       return globalBin;
     }
 
-    ALPAKA_FN_HOST_ACC inline constexpr int getGlobalBinByBin(const TAcc& acc, const VecArray<int, Ndim>& Bins) const {
-      int globalBin{Bins[0]};
+	template <typename TAcc>
+    ALPAKA_FN_HOST_ACC inline constexpr int getGlobalBinByBin(const TAcc& acc, const VecArray<uint32_t, Ndim>& Bins) const {
+      uint32_t globalBin{Bins[0]};
       int nTilesPerDim{(int)(alpaka::math::pow(acc, n_tiles, 1.0 / Ndim))};
       for (int i{1}; i != Ndim; ++i) {
         globalBin += nTilesPerDim * Bins[i];
@@ -72,19 +73,21 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
       return globalBin;
     }
 
+	template <typename TAcc>
     ALPAKA_FN_ACC inline constexpr void fill(const TAcc& acc, const VecArray<float, Ndim>& coords, int i) {
-      m_tiles[getGlobalBin(coords)].push_back(acc, i);
+      m_tiles[getGlobalBin(acc, coords)].push_back(acc, i);
     }
 
+	template <typename TAcc>
     ALPAKA_FN_ACC inline void searchBox(const TAcc& acc,
                                         const VecArray<VecArray<float, 2>, Ndim>& sb_extremes,
                                         VecArray<VecArray<uint32_t, 2>, Ndim>* search_box) {
       for (int dim{}; dim != Ndim; ++dim) {
         VecArray<uint32_t, 2> dim_sb;
-        dim_sb.push_back(acc, getBin(sb_extremes[dim][0], dim));
-        dim_sb.push_back(acc, getBin(sb_extremes[dim][1], dim));
+        dim_sb.push_back(acc, getBin(acc, sb_extremes[dim][0], dim));
+        dim_sb.push_back(acc, getBin(acc, sb_extremes[dim][1], dim));
 
-        search_box->push_back_unsafe(acc, dim_sb);
+        search_box->push_back(acc, dim_sb);
       }
     }
 
@@ -101,9 +104,9 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
     }
 
   private:
-    TAcc m_acc;
-    cms::alpakatools::host_buffer<VecArray<uint32_t, max_tile_depth>> m_tiles;
+    VecArray<VecArray<uint32_t, max_tile_depth>, max_n_tiles> m_tiles;
   };
+
 
 }  // namespace ALPAKA_ACCELERATOR_NAMESPACE
 
