d53c7b26613c9dbec2829e6e5e8d057d1c0b69c0
sbaldu <simone.balducci00@gmail.com>
2023-08-08 18:50:34 +0200

    Add resize of output vectors in Points constructor

--- CLUEstering/alpaka/DataFormats/Points.h
+++ CLUEstering/alpaka/DataFormats/Points.h
@@ -15,16 +15,34 @@ using cms::alpakatools::VecArray;
 template <uint8_t Ndim>
 struct Points {
   Points() = default;
+  Points(const std::vector<VecArray<float, Ndim>>& coords, const std::vector<float>& weight)
+      : m_coords{coords}, m_weight{weight}, n{weight.size()} {}
+  Points(const std::vector<std::vector<float>>& coords, const std::vector<float>& weight)
+      : m_weight{weight}, n{weight.size()} {
+		for (const auto& x : coords) {
+		  VecArray<float, Ndim> temp_vecarray;
+		  for (auto value : x) {
+			temp_vecarray.push_back_unsafe(value);
+		  }
+		  m_coords.push_back(temp_vecarray);
+		}
 
-  std::vector<VecArray<float, Ndim>> coords;
-  std::vector<float> weight;
-  std::vector<float> rho;
-  std::vector<float> delta;
-  std::vector<int> nearestHigher;
-  std::vector<int> clusterIndex;
-  std::vector<int> isSeed;
+		m_rho.resize(n);
+		m_delta.resize(n);
+		m_nearestHigher.resize(n);
+		m_clusterIndex.resize(n);
+		m_isSeed.resize(n);
+	  }
 
-  int n;
+  std::vector<VecArray<float, Ndim>> m_coords;
+  std::vector<float> m_weight;
+  std::vector<float> m_rho;
+  std::vector<float> m_delta;
+  std::vector<int> m_nearestHigher;
+  std::vector<int> m_clusterIndex;
+  std::vector<int> m_isSeed;
+
+  size_t n;
 };
 
 #endif
