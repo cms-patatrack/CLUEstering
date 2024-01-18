cbaab19b3a1cc2d001bf400bbed2f89e94740b0c
sbaldu <simone.balducci00@gmail.com>
2024-01-18 10:11:19 +0100

    Update `setup.py` to use CMake when building the wheel

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
+
+import os
 from pathlib import Path
-from setuptools import setup, find_packages
-from pybind11.setup_helpers import Pybind11Extension
+from setuptools import Extension, setup, find_packages
 
-__version__ = "1.4.0"
+__version__ = "2.0.0"
 this_directory = Path(__file__).parent
-long_description = (this_directory / 'README.md').read_text()
+long_description = (this_directory/'README.md').read_text()
 
-ext_modules = [
-    Pybind11Extension(
-        "CLUEsteringCPP",
-        ['CLUEstering/binding.cc'],
-        include_dirs=['CLUEstering/include/']
-    ),
-]
+print("Now we build with cmake")
+os.system("cmake -B build && make -C build")
 
 setup(
     name="CLUEstering",
@@ -21,18 +17,18 @@ setup(
     author_email="simone.balducci00@gmail.com",
     description='''A library that generalizes the original 2-dimensional CLUE
 				 algorithm made at CERN.''',
-         long_description=long_description,
-         long_description_content_type='text/markdown',
-         packages=find_packages(),
-         install_requires=['scikit-learn', 'numpy', 'matplotlib', 'pandas'],
-         ext_modules=ext_modules,
-         keywords=['Python', 'Clustering', 'Binding'],
-         python_requires='>=3.7',
-         classifiers=[
-                'Intended Audience :: Developers',
-                'Programming Language :: Python :: 3',
-                'Operating System :: Unix',
-                'Operating System :: MacOS :: MacOS X',
-                'Operating System :: Microsoft :: Windows',
-         ]
+	 long_description=long_description,
+	 long_description_content_type='text/markdown',
+	 packages=find_packages(),
+	 install_requires=['scikit-learn','numpy','matplotlib','pandas'],
+     package_data={'': ['*.so']},
+	 keywords=['Python','Clustering','Binding'],
+	 python_requires='>=3.7',
+	 classifiers=[
+		'Intended Audience :: Developers',
+		'Programming Language :: Python :: 3',
+		'Operating System :: Unix',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: Microsoft :: Windows',
+	 ]
 )
