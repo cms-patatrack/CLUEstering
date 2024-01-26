3aef2d7ab6ee297ba8d793ecc6e459558818c3e6
sbaldu <simone.balducci00@gmail.com>
2024-01-26 22:22:27 +0100

    Fix packaging and installation

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 
 import os
+import sys
 from pathlib import Path
-from setuptools import Extension, setup, find_packages
+from setuptools import setup
 
 __version__ = "2.0.0"
 this_directory = Path(__file__).parent
 long_description = (this_directory/'README.md').read_text()
 
-print("Now we build with cmake")
-os.system("cmake -B build && make -C build")
+if sys.argv[1] != 'sdist':
+    os.system("cmake -B build && make -C build")
 
 setup(
     name="CLUEstering",
@@ -16,19 +17,19 @@ setup(
     author="Simone Balducci",
     author_email="simone.balducci00@gmail.com",
     description='''A library that generalizes the original 2-dimensional CLUE
-				 algorithm made at CERN.''',
-	 long_description=long_description,
-	 long_description_content_type='text/markdown',
-	 packages=find_packages(),
-	 install_requires=['scikit-learn','numpy','matplotlib','pandas'],
-     package_data={'': ['lib/*.so']},
-	 keywords=['Python','Clustering','Binding'],
-	 python_requires='>=3.7',
-	 classifiers=[
+				algorithm made at CERN.''',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['CLUEstering'],
+    install_requires=['scikit-learn','numpy','matplotlib','pandas'],
+    package_data={'': []},
+    keywords=['Python','Clustering','Binding'],
+    python_requires='>=3.7',
+    classifiers=[
 		'Intended Audience :: Developers',
 		'Programming Language :: Python :: 3',
 		'Operating System :: Unix',
 		'Operating System :: MacOS :: MacOS X',
 		'Operating System :: Microsoft :: Windows',
-	 ]
+    ]
 )
