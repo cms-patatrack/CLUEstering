f136c5e94839afd4edabdc60a31db656c9c5eb1e
sbaldu <simone.balducci00@gmail.com>
2024-02-19 11:31:00 +0100

    Fix typo in check for modules

--- CLUEstering/CLUEstering.py
+++ CLUEstering/CLUEstering.py
@@ -20,13 +20,13 @@ path = dirname(__file__)
 sys.path.insert(1, join(path, 'lib'))
 import CLUE_Convolutional_Kernels as clue_kernels
 import CLUE_CPU_Serial as cpu_serial
-tbb_found = exists(*glob(join(path, 'lib/CLUE_CPU_TBB*.so')))
+tbb_found = exists(str(*glob(join(path, 'lib/CLUE_CPU_TBB*.so'))))
 if tbb_found:
     import CLUE_CPU_TBB as cpu_tbb
-cuda_found = exists(*glob(join(path, 'lib/CLUE_CPU_TBB*.so')))
+cuda_found = exists(str(*glob(join(path, 'lib/CLUE_CPU_CUDA*.so'))))
 if cuda_found:
     import CLUE_GPU_CUDA as gpu_cuda
-hip_found = exists(*glob(join(path, 'lib/CLUE_CPU_TBB*.so')))
+hip_found = exists(str(*glob(join(path, 'lib/CLUE_CPU_HIP*.so'))))
 if hip_found:
     import CLUE_GPU_HIP as gpu_hip
 
