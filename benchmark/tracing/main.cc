fb9bd9dcb18751ed9f6c0a1022ca16cbe792ecb9
Simone Balducci <simone.balducci00@gmail.com>
2024-07-12 09:22:56 +0200

    Add tracing folder inside benchmark

---
+++ benchmark/tracing/main.cc
@@ -0,0 +1,45 @@
+
+#include <alpaka/alpaka.hpp>
+#include <algorithm>
+#include <chrono>
+#include <ostream>
+#include <vector>
+
+#include "CLUE/CLUEAlgoAlpaka.h"
+#include "CLUE/Run.h"
+#include "DataFormats/Points.h"
+#include "DataFormats/alpaka/PointsAlpaka.h"
+
+#include "read_csv.hpp"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+  void run() {
+    const auto dev_acc = alpaka::getDevByIdx<Acc1D>(0u);
+    //const auto dev_acc = cms::alpakatools::devices<alpaka::Pltf<Device>>()[0];
+    //const auto dev_acc = alpaka::getDevs<Platform>()[0];
+    Queue queue_(dev_acc);
+    const auto data{read_csv<float, 2>("./sissa.csv")};
+    Points<2> h_points(data.first, data.second);
+    PointsAlpaka<2> d_points(queue_, data.second.size());
+
+    const float dc{3.f}, rhoc{2.f}, outlier{3.5f};
+    const int pPBin{128};
+    CLUEAlgoAlpaka<Acc1D, 2> algo(dc, rhoc, outlier, pPBin, queue_);
+
+    const std::size_t block_size{256};
+	auto result = algo.make_clusters(h_points, d_points, FlatKernel{.5f}, queue_, block_size);
+  }
+};  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+int main(int argc, char* argv[]) {
+  int nruns{1};
+  if (argc == 2) {
+	nruns = std::stoi(argv[1]);
+
+  }
+
+  std::cout << "setup,HtD,CLD,CNH,FC,AC,DtH\n";
+  for (int i{}; i < nruns; ++i) {
+	ALPAKA_ACCELERATOR_NAMESPACE::run();
+  }
+}
