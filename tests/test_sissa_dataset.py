67d556718f086dba74749bb079a4c6362cb8ed3e
Simone Balducci <simone.balducci00@gmail.com>
2024-07-15 15:09:12 +0200

    Update parameters in tests

--- tests/test_sissa_dataset.py
+++ tests/test_sissa_dataset.py
@@ -1,6 +1,6 @@
 '''
-Testing the algorithm on the circle dataset, a dataset where points are distributed to form
-many small clusters
+Testing the algorithm on the circle dataset, a dataset where points are
+distributed to form many small clusters
 '''
 
 import os
@@ -10,6 +10,7 @@ import pytest
 from check_result import check_result
 sys.path.insert(1, '../CLUEstering/')
 import CLUEstering as clue
+
 
 @pytest.fixture
 def sissa():
@@ -21,14 +22,15 @@ def sissa():
 
 def test_sissa_clustering(sissa):
     '''
-    Checks that the output of the clustering is the one given by the truth dataset
+    Checks that the output of the clustering is the one given by the
+    truth dataset.
     '''
 
     # Check if the output file already exists and if it does, delete it
     if os.path.isfile('./sissa_output.csv'):
         os.remove('./sissa_output.csv')
 
-    c = clue.clusterer(0.4, 5, 1.)
+    c = clue.clusterer(20., 10., 1.)
     c.read_data(sissa)
     c.run_clue()
     c.to_csv('./', 'sissa_output.csv')
@@ -36,8 +38,9 @@ def test_sissa_clustering(sissa):
     check_result('./sissa_output.csv',
                  './test_datasets/truth_files/sissa_1000_truth.csv')
 
+
 if __name__ == "__main__":
-    c = clue.clusterer(0.8, 5, 1.5)
+    c = clue.clusterer(20., 10., 1.)
     c.read_data("./test_datasets/sissa.csv")
     c.run_clue()
     c.cluster_plotter()
