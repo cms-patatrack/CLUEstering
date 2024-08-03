c761a882ae59e241b5511539ab2ecd86d53b6d01
Simone Balducci <simone.balducci00@gmail.com>
2024-08-03 17:58:55 +0200

    Remove debug compilation warnings

--- CLUEstering/alpaka/CLUE/ConvolutionalKernel.h
+++ CLUEstering/alpaka/CLUE/ConvolutionalKernel.h
@@ -21,8 +21,8 @@ public:
 
   // Overload call operator
   template <typename TAcc>
-  ALPAKA_FN_HOST_ACC float operator()(const TAcc& acc,
-                                      float dist_ij,
+  ALPAKA_FN_HOST_ACC float operator()(const TAcc&,
+                                      float /*dist_ij*/,
                                       int point_id,
                                       int j) const {
     if (point_id == j) {
