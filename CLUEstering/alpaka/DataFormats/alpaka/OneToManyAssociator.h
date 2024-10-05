293febe108ff0e9e2d009ecf7ec0e1a6bb219ec3
Simone Balducci <simone.balducci00@gmail.com>
2024-10-05 16:01:08 +0200

    Formatting

--- CLUEstering/alpaka/DataFormats/alpaka/OneToManyAssociator.h
+++ CLUEstering/alpaka/DataFormats/alpaka/OneToManyAssociator.h
@@ -42,26 +42,38 @@ public:
     }
   }
 
-  ALPAKA_FN_HOST_ACC constexpr auto size() const { return uint32_t(off[off.capacity - 1]); }
-  ALPAKA_FN_HOST_ACC constexpr auto size(uint32_t tile) const { return off[tile + 1] - off[tile]; }
+  ALPAKA_FN_HOST_ACC constexpr auto size() const {
+    return uint32_t(off[off.capacity - 1]);
+  }
+  ALPAKA_FN_HOST_ACC constexpr auto size(uint32_t tile) const {
+    return off[tile + 1] - off[tile];
+  }
 
   ALPAKA_FN_HOST_ACC constexpr const point_index* begin() const { return content.data(); }
   ALPAKA_FN_HOST_ACC constexpr point_index* begin() { return content.data(); }
   ALPAKA_FN_HOST_ACC constexpr const point_index* end() const { return begin() + size(); }
   ALPAKA_FN_HOST_ACC constexpr point_index* end() { return begin() + size(); }
 
-  ALPAKA_FN_HOST_ACC constexpr const point_index* begin(uint32_t b) const { return content.data() + off[b]; }
-  ALPAKA_FN_HOST_ACC constexpr point_index* begin(uint32_t b) { return content.data() + off[b]; }
+  ALPAKA_FN_HOST_ACC constexpr const point_index* begin(uint32_t b) const {
+    return content.data() + off[b];
+  }
+  ALPAKA_FN_HOST_ACC constexpr point_index* begin(uint32_t b) {
+    return content.data() + off[b];
+  }
 
   ALPAKA_FN_HOST_ACC constexpr const point_index* end(uint32_t b) const {
     return content.data() + off[b + 1];
   }
-  ALPAKA_FN_HOST_ACC constexpr point_index* end(uint32_t b) { return content.data() + off[b + 1]; }
+  ALPAKA_FN_HOST_ACC constexpr point_index* end(uint32_t b) {
+    return content.data() + off[b + 1];
+  }
 
   ALPAKA_FN_HOST_ACC constexpr clue::span<const point_index> view(uint32_t b) const {
     return {begin(b), size(b)};
   }
-  ALPAKA_FN_HOST_ACC constexpr clue::span<point_index> view(uint32_t b) { return {begin(b), size(b)}; }
+  ALPAKA_FN_HOST_ACC constexpr clue::span<point_index> view(uint32_t b) {
+    return {begin(b), size(b)};
+  }
 
   FlexiStorage<Counter, ONES> off;
   FlexiStorage<point_index, SIZE> content;
