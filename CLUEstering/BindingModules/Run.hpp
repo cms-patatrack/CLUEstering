5a760ff412a7415da1b19f0af17b8d2f197e2b4e
Simone Balducci <simone.balducci00@gmail.com>
2025-01-09 09:25:02 +0100

    Move run.hpp

---
+++ CLUEstering/BindingModules/Run.hpp
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include <vector>
+#include "CLUEstering/CLUEstering.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
+
+  template <uint8_t Ndim, typename Kernel>
+  std::vector<std::vector<int>> run(float dc,
+                                    float rhoc,
+                                    float dm,
+                                    int pPBin,
+                                    const std::vector<std::vector<float>>& coordinates,
+                                    const std::vector<float>& weight,
+                                    const Kernel& kernel,
+                                    Queue queue_,
+                                    size_t block_size) {
+    CLUEAlgoAlpaka<Ndim> algo(dc, rhoc, dm, pPBin, queue_);
+
+    // Create the host and device points
+    Points<Ndim> h_points(coordinates, weight);
+    PointsAlpaka<Ndim> d_points(queue_, weight.size());
+
+    return algo.make_clusters(h_points, d_points, kernel, queue_, block_size);
+  }
+
+};  // namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE
