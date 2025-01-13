9d527d42de83d5a0ec577ab61e4af6f649b24ccb
Simone Balducci <simone.balducci00@gmail.com>
2025-01-13 10:28:17 +0100

    Add test for new clusterer import

---
+++ tests/test_clusterer_import.py
@@ -0,0 +1,47 @@
+'''
+'''
+
+from check_result import check_result
+import os
+import sys
+import pandas as pd
+import pytest
+sys.path.insert(1, '../CLUEstering/')
+import CLUEstering as clue
+
+@pytest.fixture
+def sissa():
+    '''
+    Returns the dataframe containing the sissa dataset
+    '''
+    return pd.read_csv("./test_datasets/sissa.csv")
+
+def test_clusterer_import(sissa):
+    '''
+    Try importing a clusterer from csv file and check that it's equal to the original clusterer
+    '''
+    # Check if the output file already exists and if it does, delete it
+    if os.path.isfile('./test_sissa_import.csv'):
+        os.remove('./test_sissa_import.csv')
+
+    c = clue.clusterer(20., 10., 20.)
+    c.read_data(sissa)
+    c.run_clue()
+    c.to_csv('./', 'test_sissa_import.csv')
+
+    d = clue.clusterer(20., 10., 20.)
+    d.import_clusterer('./', 'test_sissa_import.csv')
+
+    assert c.clust_prop == d.clust_prop
+
+if __name__ == "__main__":
+    c = clue.clusterer(20., 10., 20.)
+    c.read_data("./test_datasets/sissa.csv")
+    c.run_clue()
+    c.cluster_plotter()
+    c.to_csv('./', 'test_sissa_import.csv')
+
+    d = clue.clusterer(20., 10., 20.)
+    d.import_clusterer('./test_sissa_import.csv')
+
+    assert c == d
