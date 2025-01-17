14437f07bfa1f63960864f0f14518e88a92d6d9c
AuroraPerego <aurora.perego@cern.ch>
2025-01-17 16:02:05 +0100

    add support for wrapped coordinates

--- include/CLUEstering/DataFormats/alpaka/TilesAlpaka.hpp
+++ include/CLUEstering/DataFormats/alpaka/TilesAlpaka.hpp
@@ -32,6 +32,11 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
     ALPAKA_FN_HOST_ACC float& min(int i) { return m_data[2 * i]; }
     ALPAKA_FN_HOST_ACC float max(int i) const { return m_data[2 * i + 1]; }
     ALPAKA_FN_HOST_ACC float& max(int i) { return m_data[2 * i + 1]; }
+    ALPAKA_FN_HOST_ACC float range(int i) const { return max(i) - min(i); }
+    ALPAKA_FN_HOST_ACC float& range(int i) {
+      auto tmp = max(i) - min(i);
+      return tmp;
+    }
   };
 
   template <uint8_t Ndim>
@@ -52,9 +57,15 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
 
     template <typename TAcc>
     ALPAKA_FN_ACC inline constexpr int getBin(const TAcc& acc,
-                                              float coord,
-                                              int dim) const {
-      int coord_bin{(int)((coord - minmax->min(dim)) / tilesizes[dim])};
+                                              float coord_,
+                                              int dim_) const {
+      int coord_Bin;
+      if (wrapped[dim_]) {
+        coord_Bin = static_cast<int>(
+            (normalizeCoordinate(coord_, dim_) - min_max.min(dim_)) / tile_size[dim_]);
+      } else {
+        coord_Bin = static_cast<int>((coord_ - min_max.min(dim_)) / tile_size[dim_]);
+      }
 
       // Address the cases of underflow and overflow
       coord_bin = alpaka::math::min(acc, coord_bin, nperdim - 1);
@@ -80,10 +91,12 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
         const TAcc& acc, const VecArray<uint32_t, Ndim>& Bins) const {
       uint32_t global_bin = 0;
       for (int dim = 0; dim != Ndim - 1; ++dim) {
-        global_bin += alpaka::math::pow(acc, nperdim, Ndim - dim - 1) * Bins[dim];
+        auto bin_i = wrapped[dim] ? (Bins[dim] % nperdim) : Bins[dim];
+        globalBin += alpaka::math::pow(acc, nperdim, Ndim - dim - 1) * bin_i;
       }
-      global_bin += Bins[Ndim - 1];
-      return global_bin;
+      globalBin +=
+          wrapped[Ndim - 1] ? (Bins[Ndim - 1] % nperdim) : Bins[Ndim - 1];
+      return globalBin;
     }
 
     template <typename TAcc>
@@ -93,8 +106,12 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
         VecArray<VecArray<uint32_t, 2>, Ndim>* search_box) {
       for (int dim{}; dim != Ndim; ++dim) {
         VecArray<uint32_t, 2> dim_sb;
-        dim_sb.push_back_unsafe(getBin(acc, sb_extremes[dim][0], dim));
-        dim_sb.push_back_unsafe(getBin(acc, sb_extremes[dim][1], dim));
+        auto infBin = getBin(acc, sb_extremes[dim][0], dim);
+        auto supBin = getBin(acc, sb_extremes[dim][1], dim);
+        if (wrapped[dim] and infBin > supBin)
+          supBin += nperdim;
+        dim_sb.push_back_unsafe(infBin);
+        dim_sb.push_back_unsafe(supBin);
 
         search_box->push_back_unsafe(dim_sb);
       }
@@ -107,6 +124,18 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
       return clue::Span<uint32_t>{buf_ptr, size};
     }
   };
+
+    ALPAKA_FN_ACC inline float distance(const float* coord_i, const float* coord_j) {
+      float dist_sq = 0.f;
+      for (int dim = 0; dim != Ndim; ++dim) {
+        if (wrapped[dim])
+          dist_sq += normalizeCoordinate(coord_i - coord_j, dim) *
+                     normalizeCoordinate(coord_i - coord_j, dim);
+        else
+          dist_sq += (coord_i - coord_j) * (coord_i - coord_j);
+      }
+      return dist_sq;
+    }
 
   template <uint8_t Ndim>
   class TilesAlpaka {
@@ -179,11 +208,23 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
     }
 
   private:
+    ALPAKA_FN_HOST_ACC inline constexpr float normalizeCoordinate(float coord,
+                                                                  int dim) const {
+      const float range = min_max.range(dim);
+      float remainder = coord - static_cast<int>(coord / range) * range;
+      if (remainder >= min_max.max(dim))
+        remainder -= range;
+      else if (remainder < min_max.min(dim))
+        remainder += range;
+      return remainder;
+    }
+
     clue::AssociationMap<Device> m_assoc;
     clue::device_buffer<Device, CoordinateExtremes<Ndim>> m_minmax;
     clue::device_buffer<Device, float[Ndim]> m_tilesizes;
     int32_t m_ntiles;
     int32_t m_nperdim;
+    int32_t wrapped[Ndim];
     clue::device_buffer<Device, TilesAlpakaView<Ndim>> m_view;
   };
 
