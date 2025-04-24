51ea784609daa6de0a0ee18a660e28e7ac901157
Simone Balducci <simone.balducci00@gmail.com>
2025-04-24 21:22:42 +0200

    Rename Clusterer class and move it to clue namespace
    Update interface

--- CLUEstering/BindingModules/Run.hpp
+++ CLUEstering/BindingModules/Run.hpp
@@ -4,26 +4,26 @@
 #include <vector>
 #include "CLUEstering/CLUEstering.hpp"
 
-namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE {
+using ALPAKA_ACCELERATOR_NAMESPACE_CLUE::Acc1D;
+using ALPAKA_ACCELERATOR_NAMESPACE_CLUE::Device;
+using ALPAKA_ACCELERATOR_NAMESPACE_CLUE::Queue;
 
-  template <uint8_t Ndim, typename Kernel>
-  void run(float dc,
-           float rhoc,
-           float dm,
-           int pPBin,
-           std::tuple<float*, int*>&& pData,
-           uint32_t n_points,
-           const Kernel& kernel,
-           Queue queue,
-           size_t block_size) {
-    CLUEAlgoAlpaka<Ndim> algo(dc, rhoc, dm, pPBin, queue);
+template <uint8_t Ndim, typename Kernel>
+void run(float dc,
+         float rhoc,
+         float dm,
+         int pPBin,
+         std::tuple<float*, int*>&& pData,
+         uint32_t n_points,
+         const Kernel& kernel,
+         Queue queue,
+         size_t block_size) {
+  clue::Clusterer<Ndim> algo(dc, rhoc, dm, pPBin, queue);
 
-    // Create the host and device points
-    clue::PointsHost<Ndim> h_points(
-        queue, n_points, std::get<0>(pData), std::get<1>(pData));
-    clue::PointsDevice<Ndim, Device> d_points(queue, n_points);
+  // Create the host and device points
+  clue::PointsHost<Ndim> h_points(
+      queue, n_points, std::get<0>(pData), std::get<1>(pData));
+  clue::PointsDevice<Ndim, Device> d_points(queue, n_points);
 
-    algo.make_clusters(h_points, d_points, kernel, queue, block_size);
-  }
-
-};  // namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE
+  algo.make_clusters(h_points, d_points, kernel, queue, block_size);
+}
