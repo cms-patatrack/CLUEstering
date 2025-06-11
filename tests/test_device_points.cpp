00d469224a6259019ce9e7df4ec95541fb6a07e6
Simone Balducci <simone.balducci00@gmail.com>
2025-06-11 18:20:53 +0200

    Correctly check content of PointsDevice

--- tests/test_device_points.cpp
+++ tests/test_device_points.cpp
@@ -23,9 +23,10 @@ struct KernelCompareDevicePoints {
       *result = 1;
     for (auto i : alpaka::uniformElements(acc, size)) {
       int comparison = 1;
-      for (auto j = 0; j <= Ndim; ++j) {
-        comparison = (view->coords[i + Ndim * size] == d_input[i + Ndim * size]);
+      for (auto dim = 0; dim < Ndim; ++dim) {
+        comparison = (view->coords[i + dim * size] == d_input[i + dim * size]);
       }
+      comparison = (view->weight[i] == d_input[i + Ndim * size]);
 
       alpaka::atomicAnd(acc, result, comparison);
     }
@@ -34,11 +35,13 @@ struct KernelCompareDevicePoints {
 
 template <std::ranges::range TRange, uint8_t Ndim>
 ALPAKA_FN_HOST bool compareDevicePoints(Queue queue,
-                                        TRange&& h_input,
+                                        TRange&& h_coords,
+                                        TRange&& h_weights,
                                         clue::PointsDevice<Ndim, Device>& d_points,
                                         uint32_t size) {
   auto h_points = clue::PointsHost<Ndim>(queue, size);
-  std::ranges::copy(h_input, h_points.coords().begin());
+  std::ranges::copy(h_coords, h_points.coords().begin());
+  std::ranges::copy(h_weights, h_points.weights().begin());
   clue::copyToDevice(queue, d_points, h_points);
 
   // define buffers for comparison
@@ -72,29 +75,31 @@ TEST_CASE("Test device points with internal allocation") {
 
   auto to_float = [](int i) -> float { return static_cast<float>(i); };
   CHECK(compareDevicePoints(queue,
-                            std::views::iota(0, (int)(3 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(2 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(size)) | std::views::transform(to_float),
                             d_points,
                             size));
 }
 
-TEST_CASE("Test host points with external allocation of whole buffer") {
+TEST_CASE("Test device points with external allocation of whole buffer") {
   const auto device = alpaka::getDevByIdx(alpaka::Platform<Acc1D>{}, 0u);
   Queue queue(device);
 
   const uint32_t size = 1000;
-  const auto bytes = clue::soa::host::computeSoASize<2>(size);
+  const auto bytes = clue::soa::device::computeSoASize<2>(size);
   auto buffer = clue::make_device_buffer<std::byte[]>(queue, bytes);
 
   clue::PointsDevice<2, Device> d_points(queue, size, std::span(buffer.data(), bytes));
 
   auto to_float = [](int i) -> float { return static_cast<float>(i); };
   CHECK(compareDevicePoints(queue,
-                            std::views::iota(0, (int)(3 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(2 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(size)) | std::views::transform(to_float),
                             d_points,
                             size));
 }
 
-TEST_CASE("Test host points with external allocation passing the two buffers as pointers") {
+TEST_CASE("Test device points with external allocation passing the two buffers as pointers") {
   const auto device = alpaka::getDevByIdx(alpaka::Platform<Acc1D>{}, 0u);
   Queue queue(device);
 
@@ -105,18 +110,19 @@ TEST_CASE("Test host points with external allocation passing the two buffers as 
   clue::PointsDevice<2, Device> d_points(queue, size, input.data(), output.data());
   auto to_float = [](int i) -> float { return static_cast<float>(i); };
   CHECK(compareDevicePoints(queue,
-                            std::views::iota(0, (int)(3 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(2 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(size)) | std::views::transform(to_float),
                             d_points,
                             size));
 }
 
-TEST_CASE("Test host points with external allocation passing four buffers as pointers") {
+TEST_CASE("Test device points with external allocation passing four buffers as pointers") {
   const auto device = alpaka::getDevByIdx(alpaka::Platform<Acc1D>{}, 0u);
   Queue queue(device);
 
   const uint32_t size = 1000;
   auto coords = clue::make_device_buffer<float[]>(queue, 2 * size);
-  auto weights = clue::make_device_buffer<int[]>(queue, size);
+  auto weights = clue::make_device_buffer<float[]>(queue, size);
   auto cluster_ids = clue::make_device_buffer<int[]>(queue, size);
   auto b_isseed = clue::make_device_buffer<int[]>(queue, size);
 
@@ -124,7 +130,8 @@ TEST_CASE("Test host points with external allocation passing four buffers as poi
       queue, size, coords.data(), weights.data(), cluster_ids.data(), b_isseed.data());
   auto to_float = [](int i) -> float { return static_cast<float>(i); };
   CHECK(compareDevicePoints(queue,
-                            std::views::iota(0, (int)(3 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(2 * size)) | std::views::transform(to_float),
+                            std::views::iota(0, (int)(size)) | std::views::transform(to_float),
                             d_points,
                             size));
 }
