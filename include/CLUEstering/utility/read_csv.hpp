779d4488f378d69873e84eadf0654bb8b0ce0beb
Simone Balducci <simone.balducci00@gmail.com>
2025-07-01 17:17:35 +0200

    Read-csv return `pointsHost` object

--- include/CLUEstering/utility/read_csv.hpp
+++ include/CLUEstering/utility/read_csv.hpp
@@ -1,75 +1,84 @@
 
 #pragma once
 
+#include "CLUEstering/DataFormats/PointsHost.hpp"
+#include "CLUEstering/detail/concepts.hpp"
+
+#include <algorithm>
 #include <fstream>
 #include <sstream>
 #include <string>
-#include <vector>
 
-// TODO: implement overload that takes pre-allocated Points
-template <typename T, size_t NDim>
-inline std::vector<T> read_csv(const std::string& file_path) {
-  std::fstream file(file_path);
-  if (!file.is_open()) {
-    throw std::runtime_error("Could not open file: " + file_path);
-  }
-  auto n_points =
-      std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n') - 1;
-  std::vector<T> coords((NDim + 1) * n_points);
-
-  file = std::fstream(file_path);
-  // discard the header
-  std::string buffer;
-  getline(file, buffer);
-  auto point_id = 0;
-  while (getline(file, buffer)) {
-    std::stringstream buffer_stream(buffer);
-    std::string value;
-
-    for (size_t dim = 0; dim <= NDim; ++dim) {
-      getline(buffer_stream, value, ',');
-      coords[point_id + dim * n_points] = static_cast<T>(std::stod(value));
+namespace clue {
+  template <size_t NDim, concepts::queue TQueue>
+  inline clue::PointsHost<NDim> read_csv(TQueue& queue, const std::string& file_path) {
+    std::fstream file(file_path);
+    if (!file.is_open()) {
+      throw std::runtime_error("Could not open file: " + file_path);
     }
-    ++point_id;
-  }
+    auto n_points =
+        std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n') -
+        1;
 
-  file.close();
+    clue::PointsHost<NDim> points(queue, n_points);
 
-  return coords;
-}
+    file = std::fstream(file_path);
+    // discard the header
+    std::string buffer;
+    getline(file, buffer);
+    auto point_id = 0;
+    while (getline(file, buffer)) {
+      std::stringstream buffer_stream(buffer);
+      std::string value;
 
-template <size_t NDim>
-inline std::vector<int> read_output(const std::string& file_path) {
-  std::fstream file(file_path);
-  if (!file.is_open()) {
-    throw std::runtime_error("Could not open file: " + file_path);
-  }
-  auto n_points =
-      std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n') - 1;
-  std::vector<int> results(2 * n_points);
-
-  file = std::fstream(file_path);
-  // discard the header
-  std::string buffer;
-  getline(file, buffer);
-  auto point_id = 0;
-  while (getline(file, buffer)) {
-    std::stringstream buffer_stream(buffer);
-    std::string value;
-
-    for (size_t dim = 0; dim < NDim; ++dim) {
-      getline(buffer_stream, value, ',');
+      for (size_t dim = 0; dim < NDim; ++dim) {
+        getline(buffer_stream, value, ',');
+        points.coords(dim)[point_id] = std::stof(value);
+      }
+      getline(buffer_stream, value);
+      points.weights()[point_id] = std::stof(value);
+      ++point_id;
     }
-    getline(buffer_stream, value, ',');  // discard the weight value
-    getline(buffer_stream, value, ',');
-    results[point_id] = std::stoi(value);
-    getline(buffer_stream, value);
-    results[point_id + n_points] = std::stoi(value);
+    file.close();
 
-    ++point_id;
+    return points;
   }
 
-  file.close();
+  template <size_t NDim, concepts::queue TQueue>
+  inline clue::PointsHost<NDim> read_output(TQueue& queue, const std::string& file_path) {
+    std::fstream file(file_path);
+    if (!file.is_open()) {
+      throw std::runtime_error("Could not open file: " + file_path);
+    }
+    auto n_points =
+        std::count(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), '\n') -
+        1;
+    clue::PointsHost<NDim> points(queue, n_points);
 
-  return results;
-}
+    file = std::fstream(file_path);
+    // discard the header
+    std::string buffer;
+    getline(file, buffer);
+    auto point_id = 0;
+    while (getline(file, buffer)) {
+      std::stringstream buffer_stream(buffer);
+      std::string value;
+
+      for (size_t dim = 0; dim < NDim; ++dim) {
+        getline(buffer_stream, value, ',');
+        points.coords(dim)[point_id] = std::stof(value);
+      }
+      getline(buffer_stream, value, ',');
+      points.weights()[point_id] = std::stof(value);
+      getline(buffer_stream, value, ',');
+      points.clusterIndexes()[point_id] = std::stoi(value);
+      getline(buffer_stream, value);
+      points.isSeed()[point_id] = std::stoi(value);
+
+      ++point_id;
+    }
+    file.close();
+
+    return points;
+  }
+}  // namespace clue
--- tests/test_clusterer.cpp
+++ tests/test_clusterer.cpp
@@ -13,27 +13,24 @@
 using namespace ALPAKA_ACCELERATOR_NAMESPACE_CLUE;
 
 TEST_CASE("Test make_cluster interfaces") {
-  auto coords = read_csv<float, 2>("../data/data_32768.csv");
-  const uint32_t n_points = coords.size() / 3;
-  std::vector<int> results(2 * n_points);
-
   const auto dev_acc = alpaka::getDevByIdx(alpaka::Platform<Acc1D>{}, 0u);
   Queue queue(dev_acc);
 
-  clue::PointsHost<2> h_points(queue, n_points, coords, results);
+  clue::PointsHost<2> h_points = clue::read_csv<2>(queue, "../data/data_32768.csv");
+  const auto n_points = h_points.size();
   clue::PointsDevice<2, Device> d_points(queue, n_points);
 
   const float dc{1.5f}, rhoc{10.f}, outlier{1.5f};
   clue::Clusterer<2> algo(queue, dc, rhoc, outlier);
   const std::size_t block_size{256};
 
-  const auto truth_data = read_output<2>("../data/truth_files/data_32768_truth.csv");
-  auto truth_ids = std::span<const int>{truth_data.data(), n_points};
-  auto truth_isSeed = std::span<const int>{truth_data.data() + n_points, n_points};
+  const auto truth_data = clue::read_output<2>(queue, "../data/truth_files/data_32768_truth.csv");
+  auto truth_ids = truth_data.clusterIndexes();
+  auto truth_isSeed = truth_data.isSeed();
   SUBCASE("Run clustering without passing device points") {
     algo.make_clusters(h_points, FlatKernel{.5f}, queue, block_size);
-    auto clusters = std::span<const int>{results.data(), n_points};
-    auto isSeed = std::span<const int>(results.data() + n_points, n_points);
+    auto clusters = h_points.clusterIndexes();
+    auto isSeed = h_points.isSeed();
 
     CHECK(clue::validate_results(clusters, truth_ids));
     CHECK(std::ranges::equal(truth_isSeed, isSeed));
@@ -41,8 +38,8 @@ TEST_CASE("Test make_cluster interfaces") {
 
   SUBCASE("Run clustering without passing the queue") {
     algo.make_clusters(h_points, d_points, FlatKernel{.5f}, block_size);
-    auto clusters = std::span<const int>{results.data(), n_points};
-    auto isSeed = std::span<const int>(results.data() + n_points, n_points);
+    auto clusters = h_points.clusterIndexes();
+    auto isSeed = h_points.isSeed();
 
     CHECK(clue::validate_results(clusters, truth_ids));
     CHECK(std::ranges::equal(truth_isSeed, isSeed));
@@ -50,8 +47,8 @@ TEST_CASE("Test make_cluster interfaces") {
 
   SUBCASE("Run clustering without passing the queue and device points") {
     algo.make_clusters(h_points, FlatKernel{.5f}, block_size);
-    auto clusters = std::span<const int>{results.data(), n_points};
-    auto isSeed = std::span<const int>(results.data() + n_points, n_points);
+    auto clusters = h_points.clusterIndexes();
+    auto isSeed = h_points.isSeed();
 
     CHECK(clue::validate_results(clusters, truth_ids));
     CHECK(std::ranges::equal(truth_isSeed, isSeed));
