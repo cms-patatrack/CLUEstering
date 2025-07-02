639cdb29ab9fb4c6a63b47c778700f5245c33456
Simone Balducci <93096843+sbaldu@users.noreply.github.com>
2025-07-02 17:18:48 +0200

    `clue::read_csv` return `clue::pointsHost` object (#141)

--- benchmark/profiling/main.cpp
+++ benchmark/profiling/main.cpp
@@ -16,15 +16,11 @@ using ALPAKA_ACCELERATOR_NAMESPACE_CLUE::Device;
 using ALPAKA_ACCELERATOR_NAMESPACE_CLUE::Queue;
 
 void run(const std::string& input_file) {
-  auto coords = read_csv<float, 2>(input_file);
-  const auto n_points = coords.size() / 3;
-  std::vector<int> results(2 * n_points);
-
   const auto dev_acc = alpaka::getDevByIdx(alpaka::Platform<Acc1D>{}, 0u);
   Queue queue(dev_acc);
 
-  clue::PointsHost<2> h_points(queue, n_points, coords.data(), results.data());
-  clue::PointsDevice<2, Device> d_points(queue, n_points);
+  auto h_points = clue::read_csv<2>(queue, input_file);
+  clue::PointsDevice<2, Device> d_points(queue, h_points.size());
 
   const float dc{1.5f}, rhoc{10.f}, outlier{1.5f};
   clue::Clusterer<2> algo(queue, dc, rhoc, outlier);
