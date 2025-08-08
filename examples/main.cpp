2a1c4fb9dc504afb85d8f53481e1db88f9ba6cca
Simone Balducci <simone.balducci00@gmail.com>
2025-08-08 16:29:01 +0200

    Add minimal example on how to use the library

---
+++ examples/main.cpp
@@ -0,0 +1,50 @@
+
+#include <alpaka/alpaka.hpp>
+#include <CLUEstering/CLUEstering.hpp>
+
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+using Platform = alpaka::PlatformCudaRt;
+using Queue = alpaka::QueueCudaRtNonBlocking;
+
+#elif defined(ALPAKA_ACC_GPU_HIP_ENABLED)
+using Platform = alpaka::PlatformHipRt;
+using Queue = alpaka::QueueHipRtNonBlocking;
+
+#elif defined(ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED)
+using Platform = alpaka::PlatformCpu;
+using Queue = alpaka::QueueCpuBlocking;
+
+#elif defined(ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED)
+using Platform = alpaka::PlatformCpu;
+using Queue = alpaka::QueueCpuNonBlocking;
+
+#elif defined(ALPAKA_ACC_CPU_B_OMP2_T_SEQ_ENABLED)
+using Platform = alpaka::PlatformCpu;
+using Queue = alpaka::QueueCpuBlocking;
+
+#else
+#error "No supported accelerator found. Please enable a GPU or CPU backend."
+#endif
+
+int main() {
+  // Obtain the queue, which is used for allocations and kernel launches.
+  auto queue = clue::get_queue(0u);
+
+  // Allocate the points on the host and device.
+  clue::PointsHost<2> h_points = clue::read_csv<2>(queue, "../data/sissa.csv");
+  clue::PointsDevice<2> d_points(queue, h_points.size());
+
+  // Define the parameters for the clustering and construct the clusterer.
+  const float dc = 20.f, rhoc = 10.f, outlier = 20.f;
+  clue::Clusterer<2> algo(queue, dc, rhoc, outlier);
+
+  // Launch the clustering
+  // The results will be stored in the `clue::PointsHost` object
+  const std::size_t block_size{256};
+  algo.make_clusters(h_points, d_points, clue::FlatKernel{.5f}, queue, block_size);
+  // Read the data from the host points
+  auto clusters_indexes = h_points.clusterIndexes();  // Get the cluster index for each points
+  auto seed_map =
+      h_points.isSeed();  // Obtain a boolean array indicating which points are the seeds
+                          // i.e. the cluster centers
+}
