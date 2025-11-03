57cedb792538fe81be9f325b2d8c3c0a32f936f4
Simone Balducci <simone.balducci00@gmail.com>
2025-11-03 17:05:16 +0100

    Fix warnings about assert usage in PointsCommon and PointsHost

--- include/CLUEstering/data_structures/internal/PointsCommon.hpp
+++ include/CLUEstering/data_structures/internal/PointsCommon.hpp
@@ -38,14 +38,14 @@ namespace clue {
       }
 
       ALPAKA_FN_HOST auto clusterIndexes() const {
-        assert(("The points have not been clustered yet, so the cluster indexes cannot be accessed",
-                static_cast<const TPoints&>(*this).m_clustered));
+        assert(static_cast<const TPoints&>(*this).m_clustered &&
+               "The points have not been clustered yet, so the cluster indexes cannot be accessed");
         auto& view = static_cast<const TPoints*>(this)->m_view;
         return std::span<const int>(view.cluster_index, view.n);
       }
       ALPAKA_FN_HOST auto clusterIndexes() {
-        assert(("The points have not been clustered yet, so the cluster indexes cannot be accessed",
-                static_cast<const TPoints&>(*this).m_clustered));
+        assert(static_cast<const TPoints&>(*this).m_clustered &&
+               "The points have not been clustered yet, so the cluster indexes cannot be accessed");
         auto& view = static_cast<TPoints*>(this)->m_view;
         return std::span<int>(view.cluster_index, view.n);
       }
