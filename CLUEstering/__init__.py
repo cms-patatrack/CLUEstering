bd53d9c0bd9486d7d978fb71bb2ec34ebbdf3e54
Simone Balducci <simone.balducci00@gmail.com>
2025-11-04 16:43:45 +0100

    Update versions for release 2.8.0

--- CLUEstering/__init__.py
+++ CLUEstering/__init__.py
@@ -2,4 +2,4 @@ from CLUEstering.CLUEstering import clusterer
 from CLUEstering.CLUEstering import test_blobs
 from CLUEstering.CLUEstering import backends
 
-__version__ = "2.7.2"
+__version__ = "2.8.0"
