7eaf9f04d058872c02cd444279fe52f9f49a5c07
Simone Balducci <simone.balducci00@gmail.com>
2025-11-09 00:13:54 +0100

    Mark apply as host device

--- include/CLUEstering/internal/meta/apply.hpp
+++ include/CLUEstering/internal/meta/apply.hpp
@@ -3,12 +3,13 @@
 
 #include <type_traits>
 #include <utility>
+#include <alpaka/alpaka.hpp>
 
 namespace clue::meta {
 
   template <std::size_t N, typename F>
     requires std::is_invocable_r_v<void, F()>
-  void apply(F&& f) {
+  ALPAKA_FN_HOST_ACC inline constexpr void apply(F&& f) {
     [&]<std::size_t... Ids>(std::index_sequence<Ids...>) -> void {
       (f.template operator()<Ids>(), ...);
     }(std::make_index_sequence<N>{});
