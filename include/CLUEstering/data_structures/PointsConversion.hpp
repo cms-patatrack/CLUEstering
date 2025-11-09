45f6e09d205c05d8cd64b488526f34fe19ae07de
Simone Balducci <simone.balducci00@gmail.com>
2025-11-09 21:32:29 +0100

    Feature overload of `copyTo` methods returning points

--- include/CLUEstering/data_structures/PointsConversion.hpp
+++ include/CLUEstering/data_structures/PointsConversion.hpp
@@ -25,6 +25,18 @@ namespace clue {
                   PointsHost<Ndim>& h_points,
                   const PointsDevice<Ndim, TDev>& d_points);
 
+  /// @brief Copies the results of the clustering from the device points to
+  /// the host points
+  ///
+  /// @tparam TQueue The type of the queue for the device operations
+  /// @tparam Ndim The number of dimensions of the points
+  /// @tparam TDev The type of device that the points are allocated on
+  /// @param queue The queue used for the device operations
+  /// @param h_points The points allocated on the host, where the clustering results will be saved
+  /// @param d_points The points allocated on the device, where the clustering has been run
+  template <concepts::queue TQueue, std::size_t Ndim, concepts::device TDev>
+  auto copyToHost(TQueue& queue, const PointsDevice<Ndim, TDev>& d_points);
+
   /// @brief Copies the coordinates and weights of the points from the host to the device
   ///
   /// @tparam TQueue The type of the queue for the device operations
@@ -38,5 +50,17 @@ namespace clue {
   void copyToDevice(TQueue& queue,
                     PointsDevice<Ndim, TDev>& d_points,
                     const PointsHost<Ndim>& h_points);
+
+  /// @brief Copies the coordinates and weights of the points from the host to the device
+  ///
+  /// @tparam TQueue The type of the queue for the device operations
+  /// @tparam Ndim The number of dimensions of the points
+  /// @tparam TDev The type of device that the points are allocated on
+  /// @param queue The queue used for the device operations
+  /// @param d_points The empty points allocated on the device
+  /// @param h_points The points allocated on the host, containing the points' coordinates
+  /// and weights
+  template <concepts::queue TQueue, std::size_t Ndim, concepts::device TDev>
+  auto copyToDevice(TQueue& queue, const PointsHost<Ndim>& h_points);
 
 }  // namespace clue
--- include/CLUEstering/data_structures/detail/PointsConversion.hpp
+++ include/CLUEstering/data_structures/detail/PointsConversion.hpp
@@ -22,6 +22,19 @@ namespace clue {
   }
 
   template <concepts::queue TQueue, std::size_t Ndim, concepts::device TDev>
+  inline auto copyToHost(TQueue& queue, const PointsDevice<Ndim, TDev>& d_points) {
+    PointsHost<Ndim> h_points(queue, d_points.size());
+
+    alpaka::memcpy(
+        queue,
+        make_host_view(h_points.m_view.cluster_index, h_points.size()),
+        make_device_view(alpaka::getDev(queue), d_points.m_view.cluster_index, h_points.size()));
+    h_points.mark_clustered();
+
+    return h_points;
+  }
+
+  template <concepts::queue TQueue, std::size_t Ndim, concepts::device TDev>
   inline void copyToDevice(TQueue& queue,
                            PointsDevice<Ndim, TDev>& d_points,
                            const PointsHost<Ndim>& h_points) {
@@ -34,6 +47,23 @@ namespace clue {
     alpaka::memcpy(queue,
                    make_device_view(alpaka::getDev(queue), d_points.m_view.weight, h_points.size()),
                    make_host_view(h_points.m_view.weight, h_points.size()));
+  }
+
+  template <concepts::queue TQueue, std::size_t Ndim, concepts::device TDev>
+  inline auto copyToDevice(TQueue& queue, const PointsHost<Ndim>& h_points) {
+    PointsDevice<Ndim, TDev> d_points(queue, h_points.size());
+
+    meta::apply<Ndim>([&]<std::size_t Dim> {
+      alpaka::memcpy(
+          queue,
+          make_device_view(alpaka::getDev(queue), d_points.m_view.coords[Dim], h_points.size()),
+          make_host_view(h_points.m_view.coords[Dim], Ndim * h_points.size()));
+    });
+    alpaka::memcpy(queue,
+                   make_device_view(alpaka::getDev(queue), d_points.m_view.weight, h_points.size()),
+                   make_host_view(h_points.m_view.weight, h_points.size()));
+
+    return d_points;
   }
 
 }  // namespace clue
