3e8d1f9ee7a19a52722daa62577197ca98a5b9fa
Simone Balducci <93096843+sbaldu@users.noreply.github.com>
2025-11-09 21:08:20 +0100

    Memcpy columns separately in `copyToHost` (#269)

--- include/CLUEstering/data_structures/detail/PointsConversion.hpp
+++ include/CLUEstering/data_structures/detail/PointsConversion.hpp
@@ -4,6 +4,7 @@
 
 #include "CLUEstering/data_structures/PointsHost.hpp"
 #include "CLUEstering/data_structures/PointsDevice.hpp"
+#include "CLUEstering/internal/meta/apply.hpp"
 #include "CLUEstering/detail/concepts.hpp"
 #include <alpaka/alpaka.hpp>
 
@@ -24,11 +25,12 @@ namespace clue {
   inline void copyToDevice(TQueue& queue,
                            PointsDevice<Ndim, TDev>& d_points,
                            const PointsHost<Ndim>& h_points) {
-    // TODO: copy each coordinate column separately
-    alpaka::memcpy(
-        queue,
-        make_device_view(alpaka::getDev(queue), d_points.m_view.coords[0], Ndim * h_points.size()),
-        make_host_view(h_points.m_view.coords[0], Ndim * h_points.size()));
+    meta::apply<Ndim>([&]<std::size_t Dim> {
+      alpaka::memcpy(
+          queue,
+          make_device_view(alpaka::getDev(queue), d_points.m_view.coords[Dim], h_points.size()),
+          make_host_view(h_points.m_view.coords[Dim], Ndim * h_points.size()));
+    });
     alpaka::memcpy(queue,
                    make_device_view(alpaka::getDev(queue), d_points.m_view.weight, h_points.size()),
                    make_host_view(h_points.m_view.weight, h_points.size()));
