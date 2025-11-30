8f83005185e189eac8f89b01b6ccf9cab2188aaa
Simone Balducci <simone.balducci00@gmail.com>
2025-11-30 19:44:16 +0100

    Create header for distance metrics

---
+++ include/CLUEstering/core/internal/MetricInterface.hpp
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include <alpaka/alpaka.hpp>
+#include <array>
+#include <cstddef>
+
+namespace clue::internal {
+
+  template <typename TMetric, std::size_t Ndim>
+  struct MetricInterface {
+  private:
+    using Point = std::array<float, Ndim>;
+    // using DistanceType = TMetric::DistanceType;
+
+  public:
+    ALPAKA_FN_HOST_ACC constexpr auto operator()(const Point& lhs, const Point& rhs) const {
+      return static_cast<const TMetric*>(this)->distance(lhs, rhs);
+    }
+  };
+
+}  // namespace clue::internal
