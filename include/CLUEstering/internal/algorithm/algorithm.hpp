4e4adda9347c4e58ce0f6676ee6ed450a2356683
Simone Balducci <simone.balducci00@gmail.com>
2026-01-09 11:59:44 +0100

    Update associator fill functions

--- include/CLUEstering/internal/algorithm/algorithm.hpp
+++ include/CLUEstering/internal/algorithm/algorithm.hpp
@@ -3,6 +3,6 @@
 
 #include "extrema/extrema.hpp"
 #include "reduce/reduce.hpp"
-#include "scan/inclusive_scan.hpp"
+#include "scan/prefix_scan.hpp"
 #include "scan/scan.hpp"
 #include "sort/sort.hpp"
