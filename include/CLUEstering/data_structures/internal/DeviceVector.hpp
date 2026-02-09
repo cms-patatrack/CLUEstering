e4b924914d6a536095569f827ccefced6ce21620
Simone Balducci <93096843+sbaldu@users.noreply.github.com>
2026-02-10 00:05:58 +0100

    Fix `std::size_t` literals for MSVC compatibility (#407)

--- include/CLUEstering/data_structures/internal/DeviceVector.hpp
+++ include/CLUEstering/data_structures/internal/DeviceVector.hpp
@@ -34,11 +34,11 @@ namespace clue::internal {
 
     template <clue::concepts::accelerator TAcc>
     ALPAKA_FN_ACC constexpr void push_back(const TAcc& acc, int32_t value) {
-      auto prev = alpaka::atomicAdd(acc, m_size, 1ul);
+      auto prev = alpaka::atomicAdd(acc, m_size, std::size_t{1});
       if (prev < m_capacity) {
         m_data[prev] = value;
       } else {
-        alpaka::atomicSub(acc, m_size, 1ul);
+        alpaka::atomicSub(acc, m_size, std::size_t{1});
       }
     }
   };
