b9eba7e166b220d656eb3a8da0526ded1e970c0e
Simone Balducci <simone.balducci00@gmail.com>
2026-02-13 15:02:24 +0100

    Provide `np.float64` Python bindings

--- CLUEstering/BindingModules/tbb/binding_cpu_tbb.cpp
+++ CLUEstering/BindingModules/tbb/binding_cpu_tbb.cpp
@@ -23,56 +23,12 @@ namespace alpaka_tbb_async {
     m.def("listDevices",
           &alpaka_tbb_async::listDevices,
           "List the available devices for the TBB backend");
-    m.def("mainRun", &alpaka_tbb_async::mainRun<float, clue::FlatKernel>, "mainRun");
-    m.def("mainRun", &alpaka_tbb_async::mainRun<float, clue::ExponentialKernel>, "mainRun");
-    m.def("mainRun", &alpaka_tbb_async::mainRun<float, clue::GaussianKernel>, "mainRun");
-    // m.def("mainRun",
-    //       pybind11::overload_cast<double,
-    //                               double,
-    //                               double,
-    //                               double,
-    //                               int,
-    //                               std::vector<uint8_t>,
-    //                               py::array_t<double>,
-    //                               py::array_t<int>,
-    //                               const clue::FlatKernel<double>&,
-    //                               int,
-    //                               int32_t,
-    //                               size_t,
-    //                               size_t>(
-    //           &alpaka_tbb_async::mainRun<double, clue::FlatKernel<double>>),
-    //       "mainRun");
-    // m.def("mainRun",
-    //       pybind11::overload_cast<double,
-    //                               double,
-    //                               double,
-    //                               double,
-    //                               int,
-    //                               std::vector<uint8_t>,
-    //                               py::array_t<double>,
-    //                               py::array_t<int>,
-    //                               const clue::ExponentialKernel<double>&,
-    //                               int,
-    //                               int32_t,
-    //                               size_t,
-    //                               size_t>(
-    //           &alpaka_tbb_async::mainRun<double, clue::ExponentialKernel<double>>),
-    //       "mainRun");
-    // m.def("mainRun",
-    //       pybind11::overload_cast<double,
-    //                               double,
-    //                               double,
-    //                               double,
-    //                               int,
-    //                               std::vector<uint8_t>,
-    //                               py::array_t<double>,
-    //                               py::array_t<int>,
-    //                               const clue::GaussianKernel<double>&,
-    //                               int,
-    //                               int32_t,
-    //                               size_t,
-    //                               size_t>(
-    //           &alpaka_tbb_async::mainRun<double, clue::GaussianKernel<double>>),
-    //       "mainRun");
+    m.def("mainRun", &alpaka_tbb_async::mainRun<float, clue::FlatKernel>);
+    m.def("mainRun", &alpaka_tbb_async::mainRun<float, clue::ExponentialKernel>);
+    m.def("mainRun", &alpaka_tbb_async::mainRun<float, clue::GaussianKernel>);
+
+    m.def("mainRun", &alpaka_tbb_async::mainRun<double, clue::FlatKernel>);
+    m.def("mainRun", &alpaka_tbb_async::mainRun<double, clue::ExponentialKernel>);
+    m.def("mainRun", &alpaka_tbb_async::mainRun<double, clue::GaussianKernel>);
   }
 };  // namespace alpaka_tbb_async
