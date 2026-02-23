b201a1fe9e30a2e598af91eac68292b6c33fbd2e
Simone Balducci <simone.balducci00@gmail.com>
2026-02-23 10:52:27 +0100

    Bind mainRun overload for batched execution

--- CLUEstering/BindingModules/Run.hpp
+++ CLUEstering/BindingModules/Run.hpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <utility>
+#include <span>
 #include <vector>
 
 #include <pybind11/pybind11.h>
@@ -37,6 +38,34 @@ void run(TInput dc,
 
   algo.make_clusters(
       queue, h_points, d_points, clue::EuclideanMetric<Ndim, TInput>{}, kernel, block_size);
+}
+
+template <std::floating_point TInput, std::size_t Ndim, clue::concepts::convolutional_kernel Kernel>
+void run(TInput dc,
+         TInput rhoc,
+         TInput dm,
+         TInput seed_dc,
+         int pPBin,
+         std::vector<uint8_t>&& wrapped,
+         std::tuple<TInput*, int*>&& pData,
+         int32_t n_points,
+         const Kernel& kernel,
+         std::span<uint32_t> batch_sample_sizes,
+         clue::Queue queue,
+         size_t block_size) {
+  clue::Clusterer<Ndim, TInput> algo(queue, dc, rhoc, dm, seed_dc, pPBin);
+  algo.setWrappedCoordinates(std::move(wrapped));
+
+  clue::PointsHost<Ndim, TInput> h_points(queue, n_points, std::get<0>(pData), std::get<1>(pData));
+  clue::PointsDevice<Ndim, TInput> d_points(queue, n_points);
+
+  algo.make_clusters(queue,
+                     h_points,
+                     d_points,
+                     batch_sample_sizes,
+                     clue::EuclideanMetric<Ndim, TInput>{},
+                     kernel,
+                     block_size);
 }
 
 namespace ALPAKA_BACKEND {
@@ -85,6 +114,83 @@ namespace ALPAKA_BACKEND {
                                      pPBin,
                                      std::move(wrapped),
                                      std::make_tuple(pData, pResults),
+                                     n_points,
+                                     kernel,
+                                     queue,
+                                     block_size);
+    };
+    switch (Ndim) {
+      [[unlikely]] case (1):
+        dispatch.template operator()<1>();
+        return;
+      [[likely]] case (2):
+        dispatch.template operator()<2>();
+        return;
+      [[likely]] case (3):
+        dispatch.template operator()<3>();
+        return;
+      [[unlikely]] case (4):
+        dispatch.template operator()<4>();
+        return;
+      [[unlikely]] case (5):
+        dispatch.template operator()<5>();
+        return;
+      [[unlikely]] case (6):
+        dispatch.template operator()<6>();
+        return;
+      [[unlikely]] case (7):
+        dispatch.template operator()<7>();
+        return;
+      [[unlikely]] case (8):
+        dispatch.template operator()<8>();
+        return;
+      [[unlikely]] case (9):
+        dispatch.template operator()<9>();
+        return;
+      [[unlikely]] case (10):
+        dispatch.template operator()<10>();
+        return;
+      [[unlikely]] default:
+        std::cout << "This library only works up to 10 dimensions\n";
+    }
+  }
+
+  template <std::floating_point TInput, template <typename T> typename Kernel>
+    requires clue::concepts::convolutional_kernel<Kernel<TInput>>
+  void mainRun(TInput dc,
+               TInput rhoc,
+               TInput dm,
+               TInput seed_dc,
+               int pPBin,
+               std::vector<uint8_t> wrapped,
+               py::array_t<TInput> data,
+               py::array_t<int> results,
+               const Kernel<TInput>& kernel,
+               int Ndim,
+               py::array_t<uint32_t> batch_sample_sizes,
+               int32_t n_points,
+               std::size_t block_size,
+               std::size_t device_id) {
+    auto rData = data.request();
+    auto* pData = static_cast<TInput*>(rData.ptr);
+    auto rResults = results.request();
+    auto* pResults = static_cast<int*>(rResults.ptr);
+
+    auto rBatchSizes = batch_sample_sizes.request();
+    auto* pBatchSizes = static_cast<uint32_t*>(rBatchSizes.ptr);
+    std::span<uint32_t> batch_sizes(pBatchSizes, rBatchSizes.size);
+
+    auto queue = clue::get_queue(device_id);
+
+    auto dispatch = [&]<std::size_t N>() {
+      run<TInput, N, Kernel<TInput>>(dc,
+                                     rhoc,
+                                     dm,
+                                     seed_dc,
+                                     pPBin,
+                                     std::move(wrapped),
+                                     std::make_tuple(pData, pResults),
+                                     batch_sizes,
                                      n_points,
                                      kernel,
                                      queue,
