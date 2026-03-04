33c8825921d1b647e52aa0fa30ea01dc4ea65259
Simone Balducci <simone.balducci00@gmail.com>
2026-03-04 12:09:30 +0100

    Add benchmarks for different number of dimensions

---
+++ benchmark/number_of_dimensions/main.cpp
@@ -0,0 +1,36 @@
+
+#include "CLUEstering/CLUEstering.hpp"
+#include "utils/generation.hpp"
+#include <benchmark/benchmark.h>
+
+#include <cstddef>
+
+template <std::size_t Ndim>
+static void BM_clustering(benchmark::State& state) {
+  for (auto _ : state) {
+    state.PauseTiming();
+    auto queue = clue::get_queue(0u);
+    const auto n_points = static_cast<std::size_t>(state.range(0));
+
+    clue::PointsHost<Ndim> h_points(queue, n_points);
+    clue::PointsDevice<Ndim> d_points(queue, n_points);
+    clue::utils::generateRandomData<Ndim>(h_points, 20, std::make_pair(-100.f, 100.f), 1.f);
+    const auto dc = 1.5f, rhoc = 10.f, outlier = 1.5f;
+    state.ResumeTiming();
+
+    clue::Clusterer<Ndim> algo(queue, dc, rhoc, outlier);
+    algo.make_clusters(queue, h_points, d_points);
+  }
+}
+
+BENCHMARK(BM_clustering<1>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<2>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<3>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<4>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<5>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<6>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<7>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<8>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<9>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK(BM_clustering<10>)->RangeMultiplier(2)->Range(1 << 10, 1 << 19);
+BENCHMARK_MAIN();
