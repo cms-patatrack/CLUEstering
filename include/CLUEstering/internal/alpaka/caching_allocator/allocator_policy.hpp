5b213abbec19b8c41212a260df76004379fff983
Simone Balducci <simone.balducci00@gmail.com>
2026-04-15 17:16:37 +0200

    Remove internal caching allocator

--- include/CLUEstering/internal/alpaka/caching_allocator/allocator_policy.hpp
+++ include/CLUEstering/internal/alpaka/caching_allocator/allocator_policy.hpp
@@ -8,42 +8,30 @@ namespace clue {
   // Which memory allocator to use
   //   - Synchronous:   (device and host) cudaMalloc/hipMalloc and cudaMallocHost/hipMallocHost
   //   - Asynchronous:  (device only)     cudaMallocAsync (requires CUDA >= 11.2)
-  //   - Caching:       (device and host) caching allocator
-  enum class AllocatorPolicy { Synchronous = 0, Asynchronous = 1, Caching = 2 };
+  enum class AllocatorPolicy { Synchronous = 0, Asynchronous = 1 };
 
   template <typename TDev>
   constexpr inline AllocatorPolicy allocator_policy = AllocatorPolicy::Synchronous;
 
 #if defined ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED || defined ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED
   template <>
-  constexpr inline AllocatorPolicy allocator_policy<alpaka::DevCpu> =
-#if !defined ALPAKA_DISABLE_CACHING_ALLOCATOR
-      AllocatorPolicy::Caching;
-#else
-      AllocatorPolicy::Synchronous;
-#endif
+  constexpr inline AllocatorPolicy allocator_policy<alpaka::DevCpu> = AllocatorPolicy::Synchronous;
 #endif  // defined ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED || defined ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED
 
 #if defined ALPAKA_ACC_GPU_CUDA_ENABLED
   template <>
   constexpr inline AllocatorPolicy allocator_policy<alpaka::DevCudaRt> =
-#if defined CLUE_ENABLE_CACHING_ALLOCATOR
-      AllocatorPolicy::Caching;
-#elif CUDA_VERSION >= 11020 && !defined ALPAKA_DISABLE_ASYNC_ALLOCATOR
+#if CUDA_VERSION >= 11020 && !defined ALPAKA_DISABLE_ASYNC_ALLOCATOR
       AllocatorPolicy::Asynchronous;
 #else
-          AllocatorPolicy::Synchronous;
+      AllocatorPolicy::Synchronous;
 #endif
 #endif  // ALPAKA_ACC_GPU_CUDA_ENABLED
 
 #if defined ALPAKA_ACC_GPU_HIP_ENABLED
   template <>
   constexpr inline AllocatorPolicy allocator_policy<alpaka::DevHipRt> =
-#if defined CLUE_ENABLE_CACHING_ALLOCATOR
-      AllocatorPolicy::Caching;
-#else
       AllocatorPolicy::Synchronous;
-#endif
 #endif  // ALPAKA_ACC_GPU_HIP_ENABLED
 
 #if defined ALPAKA_SYCL_ONEAPI_CPU
