ea94a7c196821178e84f925a4013ddbd4da7b5a7
Simone Balducci <simone.balducci00@gmail.com>
2026-06-05 14:41:37 +0200

    fix

--- include/CLUEstering/data_structures/internal/Tiles.hpp
+++ include/CLUEstering/data_structures/internal/Tiles.hpp
@@ -115,7 +115,7 @@ namespace clue::internal {
                              size_t size) {
       auto dev = alpaka::getDev(queue);
       auto pointsView = d_points.view();
-      m_assoc.template fill<TAcc>(size, GetGlobalBin{pointsView, m_view}, queue);
+      m_assoc.template fill<TAcc>(size, GetGlobalBin(pointsView, m_view), queue);
     }
 
     template <clue::concepts::accelerator TAcc,
@@ -129,7 +129,7 @@ namespace clue::internal {
       auto dev = alpaka::getDev(queue);
       auto pointsView = d_points.view();
       m_assoc.template fill_batch<TAcc>(
-          queue, size, GetGlobalBin{pointsView, m_view}, event_offsets, max_event_size);
+          queue, size, GetGlobalBin(pointsView, m_view), event_offsets, max_event_size);
     }
 
     ALPAKA_FN_HOST inline clue::device_buffer<TDev, CoordinateExtremes<Ndim, value_type>> minMax()
